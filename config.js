dfcffd26d6e0a3c7fd29b80b08d00fd318a126b0
Hazemnabil_42 <124460984+Hazem-nabil42@users.noreply.github.com>
2024-03-19 23:06:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connect = mongoose.connect('mongodb://Wazfny.onrender.com:27017/LoginSystem');
+const connect = mongoose.connect('mongodb://wazfni.onrender.com:27017/LoginSystem');
 
 connect.then(()  => {
     console.log("Database connected successfully");
